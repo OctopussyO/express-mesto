197a1580b7734eb56b7a841a904c25399aed35d0
Julia Doroshina <up.doroshina@yandex.ru>
2020-10-30 15:56:28 +0300

    feat: add frontend

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 const express = require('express');
+const path = require('path');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.end('Hello');
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`App listening to port ${PORT}`);
