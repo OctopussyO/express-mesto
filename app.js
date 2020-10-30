7861e1924e9eb0d6e493f74b265fae0d8e42ddb9
Julia Doroshina <up.doroshina@yandex.ru>
2020-10-30 23:22:02 +0300

    feat: add routes

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const path = require('path');
+const usersRouter = require('./routes/users');
+const cardsRouter = require('./routes/cards');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.listen(PORT, () => {
-  console.log(`App listening to port ${PORT}`);
+app.use('/', usersRouter, cardsRouter);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Запрашиваемый ресурс не найден' });
 });
+
+app.listen(PORT);
